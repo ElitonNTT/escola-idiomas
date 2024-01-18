15b3c1f98a21d0868066965b6a72e55c279849da
ElitonNTT <netinho_123_anps@hotmail.com>
2024-01-18 16:14:45 -0300

    ADD: Created Footer and Header Components

---
+++ components/home/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <main className="static bottom-0 bg-green-600/85 p-4">
+      <div>footer</div>
+    </main>
+  );
+};
+
+export default Footer;
---
+++ components/home/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <main className="flex min-h-[15dvh] items-center justify-between bg-green-500/85 p-4">
+      <div>Idiomas</div>
+    </main>
+  );
+};
+
+export default Header;
