747ecdad1634501d96b3c25108257e3d5d358980
Bruno Santos <48936982+brunosanntos@users.noreply.github.com>
2024-02-02 11:01:34 -0300

    fixing type response file

--- app/api/files/route.ts
+++ app/api/files/route.ts
@@ -1,22 +1,25 @@
 import saveFile from "@/app/utils/saveFile";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 
-export async function POST(request: Request) {
-  try {
-    const data = await request.formData();
-    const file = data.get("file");
-    if (file == null || typeof file === "string") {
-      return NextResponse.json({ message: "error" }, { status: 400 });
-    }
+export async function POST(request: Request): Promise<Response> {
+  const session = await getServerSession(authOptions);
 
-    const [filePath] = await saveFile(file);
-
-    return NextResponse.json({
-      message: "success",
-      filePath,
-    });
-  } catch (error) {
-    console.log(error);
-    return NextResponse.error;
+  if (session == null) {
+    return NextResponse.json({ message: "not logged in" }, { status: 401 });
   }
+
+  const data = await request.formData();
+  const file = data.get("file");
+  if (file == null || typeof file === "string") {
+    return NextResponse.json({ message: "error" }, { status: 400 });
+  }
+
+  const [filePath] = await saveFile(file);
+
+  return NextResponse.json({
+    message: "success",
+    filePath,
+  });
 }
