ff4e40621a4ac1e29b8aba38e3054bb250f56d07
Bruno Santos <48936982+brunosanntos@users.noreply.github.com>
2024-02-15 09:06:44 -0300

    fixing layout

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,34 +1,31 @@
+import "@/app/globals.css";
+import { Footer } from "@/components/main/Footer";
+import { Header } from "@/components/main/Header";
+import { prisma } from "@/lib/prisma/prismaClient";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Centro de Idiomas",
-  description: "",
+  description: "Escola de Idiomas",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const coursesList = await prisma.course.findMany();
+
   return (
     <html lang="pt-br">
-      <Script src="/facebook.js" strategy="afterInteractive" />
-      <Script src="/tiktok.js" strategy="afterInteractive" />
-      <Script src="/tagmanager.js" strategy="afterInteractive" />
-      <Script src="/analytics.js" strategy="afterInteractive" />
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=GTM-5MGRM97"
-        strategy="afterInteractive"
-      />
-      <Script
-        strategy="afterInteractive"
-        src="https://d335luupugsy2.cloudfront.net/js/loader-scripts/bb4c0069-b896-46e7-81ca-d78821d185f0-loader.js"
-      />
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header listCourses={coursesList} />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Escola - Idiomas",
-  description: "Escola de Idiomas",
+  title: "Centro de Idiomas",
+  description: "",
 };
 
 export default function RootLayout({
@@ -15,7 +16,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
+      <Script src="/facebook.js" strategy="afterInteractive" />
+      <Script src="/tiktok.js" strategy="afterInteractive" />
+      <Script src="/tagmanager.js" strategy="afterInteractive" />
+      <Script src="/analytics.js" strategy="afterInteractive" />
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=GTM-5MGRM97"
+        strategy="afterInteractive"
+      />
+      <Script
+        strategy="afterInteractive"
+        src="https://d335luupugsy2.cloudfront.net/js/loader-scripts/bb4c0069-b896-46e7-81ca-d78821d185f0-loader.js"
+      />
       <body className={inter.className}>{children}</body>
     </html>
   );
